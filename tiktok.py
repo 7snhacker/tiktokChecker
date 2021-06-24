51b8af88cd97b291f78ae24bbae3285f1b3f74b9
7snhacker <74249353+7snhacker@users.noreply.github.com>
2021-06-24 08:25:33 +0300

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -1,7 +1,9 @@
 import requests
 import time
+
 rr = requests.session()
 from user_agent import generate_user_agent
+
 print("""
 ████████╗██╗██╗  ██╗████████╗ ██████╗ ██╗  ██╗          
 ╚══██╔══╝██║██║ ██╔╝╚══██╔══╝██╔═══██╗██║ ██╔╝          
@@ -9,32 +11,34 @@ print("""
    ██║   ██║██╔═██╗    ██║   ██║   ██║██╔═██╗           
    ██║   ██║██║  ██╗   ██║   ╚██████╔╝██║  ██╗          
    ╚═╝   ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝          
-                                                        
+
  ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗███████╗██████╗ 
 ██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝██╔════╝██╔══██╗
 ██║     ███████║█████╗  ██║     █████╔╝ █████╗  ██████╔╝
 ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██╔══╝  ██╔══██╗
 ╚██████╗██║  ██║███████╗╚██████╗██║  ██╗███████╗██║  ██║
  ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
-                                                        
+
 instagram : 7snhacker""")
 headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "User-Agent": f"{generate_user_agent()}",
-            "Connection": "close",
-            "Host": "www.tiktok.com",
-            "Accept-Encoding": "gzip, deflate",
-            "Cache-Control": "max-age=0"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "User-Agent": f"{generate_user_agent()}",
+    "Connection": "close",
+    "Host": "www.tiktok.com",
+    "Accept-Encoding": "gzip, deflate",
+    "Cache-Control": "max-age=0"
 
-            }
+}
 times = float(input('sleep : '))
-us = open('username.txt','r')
+us = open('username.txt', 'r')
 while True:
     time.sleep(times)
     user = us.readline().split("\n")[0]
     url = f"https://www.tiktok.com/@{user}"
-    req = rr.get(url,headers=headers)
+    req = rr.get(url, headers=headers)
     if req.status_code == 404:
-        print(f'{user} : Avialable')
+        print(f'{user} : Available')
+        with open('Avialable.txt',"a") as Available:
+            Available.write(user+'\n')
     elif req.status_code == 200:
-        print(f'{user} : Not Avialable')
+        print(f'{user} : Not Available')
