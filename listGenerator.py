8b533b93881715887157fe27abe731dc2d11a641
7snhacker <74249353+7snhacker@users.noreply.github.com>
2021-06-24 06:19:25 +0300

    Add files via upload

---
+++ listGenerator.py
@@ -0,0 +1,21 @@
+
+print("""  _     ___ ____ _____ ____ _____ _   _ 
+ | |   |_ _/ ___|_   _/ ___| ____| \ | |
+ | |    | |\___ \ | || |  _|  _| |  \| |
+ | |___ | | ___) || || |_| | |___| |\  |
+ |_____|___|____/ |_| \____|_____|_| \_|
+                                        """)
+print("instagram : 7snhacker")
+import string
+import random
+letters = string.ascii_lowercase
+user = int(input("letter count : "))
+r = open("username.txt", "w")
+done = 0
+li = 0
+while li == 0:
+    done += 1
+    print(done)
+    r.write(''.join(random.choice(letters) for i in range(user)) + '\n')
+
+r.close()
---
+++ tiktok.py
@@ -0,0 +1,39 @@
+import requests
+import time
+rr = requests.session()
+from user_agent import generate_user_agent
+print("""
+████████╗██╗██╗  ██╗████████╗ ██████╗ ██╗  ██╗          
+╚══██╔══╝██║██║ ██╔╝╚══██╔══╝██╔═══██╗██║ ██╔╝          
+   ██║   ██║█████╔╝    ██║   ██║   ██║█████╔╝           
+   ██║   ██║██╔═██╗    ██║   ██║   ██║██╔═██╗           
+   ██║   ██║██║  ██╗   ██║   ╚██████╔╝██║  ██╗          
+   ╚═╝   ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝          
+                                                        
+ ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗███████╗██████╗ 
+██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+██║     ███████║█████╗  ██║     █████╔╝ █████╗  ██████╔╝
+██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+╚██████╗██║  ██║███████╗╚██████╗██║  ██╗███████╗██║  ██║
+ ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                        
+instagram : 7snhacker""")
+headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "User-Agent": f"{generate_user_agent()}",
+            "Connection": "close",
+            "Host": "www.tiktok.com",
+            "Accept-Encoding": "gzip, deflate",
+            "Cache-Control": "max-age=0"
+
+            }
+times = float(input('sleep : '))
+us = open('username.txt','r')
+while True:
+    user = us.readline().split("\n")[0]
+    url = f"https://www.tiktok.com/@{user}"
+    req = rr.get(url,headers=headers)
+    if req.status_code == 404:
+        print(f'{user} : Avialable')
+    elif req.status_code == 200:
+        print(f'{user} : Not Avialable')
